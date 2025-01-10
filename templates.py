932174cf782e39a84edb5cd6fe8e3802d4ced355
Sourav Kumar <kumarsourav8432@gmail.com>
2025-01-11 01:03:46 +0530

    folder structure added

---
+++ templates.py
@@ -0,0 +1,54 @@
+import os
+from pathlib import Path
+import logging
+
+
+# logging string
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name = "CNN-Classifier"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "tests/__init__.py",
+    "tests/unit/__init__.py",
+    "tests/integration/__init__.py",
+    "configs/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "init_setup.sh",
+    "requirements.txt",
+    "requirements_dev.txt",
+    "setup.py",
+    "setup.cfg",
+    "pyproject.toml",
+    "tox.ini",
+    "research/trials.ipynb"
+] 
+
+
+for path in list_of_files:
+    path = Path(path)
+    filedir, filename = os.path.split(path)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file: {filename}")
+
+    if (not os.path.exists(path)) or (os.path.getsize(path) == 0):
+        with open(path, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {path}")
+
+    else:
+        logging.info(f"{filename} already exists")
+
+
